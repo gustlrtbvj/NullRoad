1432abcff92a6d165d98da515f5f7774d47e285f
smhrd <smhrd@DESKTOP-5RHFHQC>
2021-12-10 15:22:58 +0900

    .do

--- _NullRoad/src/main/java/FrontController/frontControllerCon.java
+++ _NullRoad/src/main/java/FrontController/frontControllerCon.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/frontController")
+@WebServlet("*.do")
 public class frontControllerCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
