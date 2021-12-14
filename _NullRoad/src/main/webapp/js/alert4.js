bec9890b513136aaf37c73db97c41b2b79c56371
smhrd <smhrd@DESKTOP-OIG44R7>
2021-12-14 15:41:39 +0900

    dfsdfsdf

---
+++ _NullRoad/src/main/webapp/js/alert4.js
@@ -0,0 +1,14 @@
+$().ready(function () {
+    $("#alertStart").click(function () {
+        Swal.fire({
+            icon: 'success',
+            confirmButtonColor: '#7e3bc3',
+            title: '주차장 등록이 신청되었습니다!',
+            text: '2-3일 내에 결과가 안내됩니다.',
+            closeOnClickOutside : false
+        }).then(function(){
+            location.href="main.html"
+            
+        });
+        });
+});
