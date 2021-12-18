41b3666f7d46158bdae02763758ea62ba26c0fcd
smhrd <smhrd@DESKTOP-C58G7I1>
2021-12-18 15:36:11 +0900

    4343

--- _NullRoad/src/main/java/Model/DAO.java
+++ _NullRoad/src/main/java/Model/DAO.java
@@ -1105,7 +1105,7 @@ public class DAO {
 		try {
 			Conn();
 
-			String sql = "select b.emdong , sum(s.user_prk_fee),AVG(s.user_prk_fee),count(s.user_prk_fee),TO_CHAR(s.chk_out_time, 'YY-MM-DD')from(select r.prk_seq, r.user_prk_fee, r.chk_out_time , p.bld_seq from t_reservation r , t_parking p  where r.prk_seq = p.prk_seq ) s, t_building b  where s.bld_seq = b.bld_seq group by b.emdong, TO_CHAR(s.chk_out_time, 'YY-MM-DD') "
+			String sql = "select b.emdong , sum(s.user_prk_fee),AVG(s.user_prk_fee),count(s.user_prk_fee),TO_CHAR(s.chk_out_time, 'YY-MM-DD')from(select r.prk_seq, r.user_prk_fee, r.chk_out_time , p.bld_seq from t_reservation r , t_parking p  where r.prk_seq = p.prk_seq and r.user_prk_fee > 0) s, t_building b  where s.bld_seq = b.bld_seq group by b.emdong, TO_CHAR(s.chk_out_time, 'YY-MM-DD') "
 					+ "order by TO_CHAR(s.chk_out_time, 'YY-MM-DD') " + "desc";
 			psmt = conn.prepareStatement(sql);
 
